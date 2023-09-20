0195b9ed3529acee754ced62cd55b50c182387e7
David Wesley <dwesley011@gmail.com>
2023-09-20 12:26:41 -0300

    Desafio DIO, mini sistema de estacionamento, concluído

---
+++ projetoEstacionamento/Estacionamento.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace projetoEstacionamento
+{
+	internal class Estacionamento // Classe estacionamento
+	{
+		public double PrecoInicial { get; set; }
+		public double PrecoPorHora { get; set; }
+
+		public List<string> Veiculos = new List<string>(); // Lista de veículos onde serão armazenados diversos valores.
+
+
+		public Estacionamento(double precoInicial, double precoporHora) // Construtor para passarmos os valores e mostrar o preço
+		{
+			PrecoInicial = precoInicial;
+			PrecoPorHora = precoporHora;
+
+		}
+
+
+
+		public void adicionarVeiculo() // Método adicionar veículos
+		{
+			Console.WriteLine(">> Opção 1 [Cadastrar veículo]");
+			Console.WriteLine();
+
+			Console.Write("Digite a Placa do veículo para estacionar: ");
+			string placaVeiculo = Console.ReadLine();
+			Veiculos.Add(placaVeiculo);
+
+		}
+
+		public void removerVeiculo() // Método remover veículos e irá perguntar a quantidade de horas no estacionamento para calcular o valor total.
+		{
+
+
+			Console.WriteLine(">> Opção 2 [Remover veículo]");
+			Console.WriteLine();
+
+			Console.Write("Digite a placa do veiculo para remover: ");
+			string placaVeiculo = Console.ReadLine();
+
+
+
+			if (Veiculos.Any()) // Verificar se o veículo existe, se existir: 
+			{
+				Console.Write("Digite a quantidade de horas que ele permaneceu estacionado: ");
+
+				int horasEstacionado = int.Parse(Console.ReadLine());
+
+				double total = (PrecoPorHora * horasEstacionado + PrecoInicial);
+
+				Veiculos.Remove(placaVeiculo);
+
+				Console.WriteLine($"O Veículo {placaVeiculo} foi removido e o total foi R$: {total}");
+
+			}
+			else
+			{
+				Console.WriteLine("O veículo informado não está cadastrado, verifique novamente.");
+
+			}
+			
+		}
+
+
+		public void listarVeiculo() // Método listar veiculos, irá mostrar os veiculos cadastrados
+		{
+			Console.WriteLine("Veiculos");
+
+			if (Veiculos.Any()) // Se existir veículos, mostre:
+			{
+				foreach (var veiculo in Veiculos)
+				{
+
+					Console.WriteLine($"Placa {veiculo}");
+				}
+
+			}
+
+			else
+			{
+				Console.WriteLine("Não há veículos estacionados. ");
+			}
+
+
+
+
+
+
+		}
+
+
+	}
+}
---
+++ projetoEstacionamento/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Globalization; // irá mudar o formato de dinheiro de vírgula, para ponto. ex: 2,35 => 2.35
+using System.Linq.Expressions;
+using projetoEstacionamento;
+internal class Program
+{
+	private static void Main(string[] args)
+	{
+		Console.WriteLine("SEJA BEM VINDO AO SISTEMA DE ESTACIONAMENTO. ");
+		Console.WriteLine();
+
+		Console.Write("Digite o preço inicial R$: ");
+		double precoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+		Console.Write("Digite o preço por hora R$: ");
+		double precoPorHora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+		Estacionamento sistemaEstacionamento = new Estacionamento(precoInicial, precoPorHora);
+
+
+		//string opcao =" ";
+		bool exibirMenu = true;
+
+
+
+		while (exibirMenu) // Enqunto a opção Exibir menu for verdadeira:
+		{
+
+			Console.Clear();
+
+			Console.WriteLine("M E N U");
+			Console.WriteLine();
+			Console.WriteLine("Digite a opção desejada:");
+			Console.WriteLine();
+			Console.WriteLine("=-=-=-=-=-=-=-=-==-=-=-=");
+			Console.WriteLine("Cadastrar veículo - 1");
+			Console.WriteLine("Remover veiculo - 2");
+			Console.WriteLine("Listar véiculos - 3");
+			Console.WriteLine("Encerrar - 4 ");
+
+			Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=");
+
+
+
+			switch (Console.ReadLine())
+			{
+				case "1":
+					sistemaEstacionamento.adicionarVeiculo();
+					break;
+
+				case "2":
+					sistemaEstacionamento.removerVeiculo();
+					break;
+
+				case "3":
+					sistemaEstacionamento.listarVeiculo();
+					break;
+
+				case "4":
+					exibirMenu = false; // Quando for false, saia do programa.
+					break;
+
+				default:
+					Console.WriteLine("Opção inválida!");
+					break;
+
+
+			}
+			Console.WriteLine();
+
+			Console.WriteLine("Pressione uma tecla para prosseguir");
+			Console.ReadLine();
+
+		}
+
+		Console.WriteLine("O programa encerrou! ");
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
